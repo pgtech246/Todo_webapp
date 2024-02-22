8464b3486ee645a423fcdab2b31f0e734da6bf18
paul <pgtech246@gmail.com>
2024-02-22 03:44:32 -0400

    Implement complete feature

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        readwrite.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Add",
               label_visibility="collapsed", 
@@ -22,4 +27,4 @@ st.text_input(label="Add",
               on_change=add_todo,
               key="new_todo")
 
-st.session_state
+# st.session_state
